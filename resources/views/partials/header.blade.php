ffb753c454ca420008e4fcc96eb2dc8e5957e81d
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-09-07 08:15:54 +0800

    final touches

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,5 +6,5 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-        <title>Lara-cart</title>
+        <title>Lara-Crud</title>
     </head>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light border-bottom">
     <div class="container">
-        <a class="navbar-brand" href="/">Lara-Cart</a>
+        <a class="navbar-brand" href="/">Lara-Crud</a>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#theNavBar" aria-controls="theNavBar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
